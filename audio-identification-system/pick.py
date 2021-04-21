54da985ca4ecede0a496036b3de333e31efd9816
Sunny Varghese <37825168+sunnyvr@users.noreply.github.com>
2021-04-21 08:46:43 +0100

    Delete audio-identification-system directory

--- audio-identification-system/pick.py
+++
@@ -1,9 +0,0 @@
-import pickle
-
-def savepickle(obj, path):
-    with open(path, 'wb') as fp:
-        pickle.dump(obj, fp)
-
-def loadpickle(path):
-    with open(path, 'rb') as fp:
-        return pickle.load(fp)
